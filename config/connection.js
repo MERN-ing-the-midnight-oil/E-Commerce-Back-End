c23a9a9acd5aa3824eb78093c0288c13c148dde2
Rhys Smoker <r.smoker@gmail.com>
2022-12-22 15:25:30 -0800

    updated proto-readme

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 //this should go to the .env file and grab DB_NAME, DB_USER and DB_PASSWORD
 require("dotenv").config();
 
+//bringing in the sequelize package, making a class, Sequelize
 const Sequelize = require("sequelize");
-
+dotenv.config();
 //this is how we did it in class in activity 01
+//using sequelize to manage our connction
 const sequelize = new Sequelize(
 	process.env.DB_NAME,
 	process.env.DB_USER,
 	process.env.DB_PASSWORD,
+	// "ecommerce_db",
+	// "root",
+	// "",
 	{
 		host: "localhost",
 		dialect: "mysql",
-		port: 3306,
+		port: 3306, //the database connection port
 	}
 );
+module.exports = sequelize;
 
 //I don't exactly understand this starter code... im assuming it does the same thing
 
@@ -31,5 +37,3 @@ const sequelize = new Sequelize(
 // 				},
 // 			}
 // 	  );
-
-module.exports = sequelize;
