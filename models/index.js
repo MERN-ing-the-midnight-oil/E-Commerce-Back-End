36d048836ea749463e36d65adc040b278ade780e
Rhys Smoker <r.smoker@gmail.com>
2023-01-02 19:52:33 -0800

    established the relationships between the product, category, tag, and productTag models in index.js in the models folder

--- models/index.js
+++ models/index.js
@@ -1,18 +1,34 @@
-// import models
+// import models //this file is about establishing relationships between models
 const Product = require("./Product");
 const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-//CHECK OUT ACTIVITY 23 THIS IS ABOUT ESTABLISHING THE RELATIONSHIPS BETWEEN TABLES , WATCH DAY 3 OF CLASS
+// establish that Product belongsTo Category
+Product.belongsTo(Category, {
+	foreingKey: "category_id",
+});
 
-// Products belongsTo Category
+// establish that a Category has many Product
+Category.hasMany(Product, {
+	foreignKey: "category_id", //category_id is a column in Product
+	onDelete: "CASCADE", //probably this deletes a category_id (in Product) when the id it references is deleted, I'm guessing.
+});
 
-// Categories have many Products
+// establish that a product can be reffered to by many Product Tags, i.e. Product belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+	through: {
+		model: ProductTag,
+		unique: false, //prevents the automatic creation of a unique through key
+	},
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+// establish that a tag can be reffred to by many products, i.e. Tag belongToMany Product (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: {
+		model: ProductTag,
+	},
+});
 
 module.exports = {
 	Product,
