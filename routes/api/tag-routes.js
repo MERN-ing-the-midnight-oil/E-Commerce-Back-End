4dbaf0fdcadccd355fc9c3bff2c0c5be7120f084
Rhys Smoker <r.smoker@gmail.com>
2023-01-04 19:01:08 -0800

    added update tags route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,12 +36,33 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 	// create a new tag
+	//localhost:3001/api/tags/
+	try {
+		const newTag = await Tag.create(req.body);
+		res.status(201).json(newTag);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.put("/:id", (req, res) => {
-	// update a tag's name by its `id` value
+// updates a tag's name by its `id` value
+router.put("/:id", async (req, res) => {
+	try {
+		const updatedTag = await Tag.update(req.body, {
+			where: {
+				id: req.params.id,
+			},
+		});
+		if (!updatedTag[0]) {
+			res.status(404).json({ message: "There is no tag with that id." });
+			return;
+		}
+		res.status(200).json(updatedTag);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.delete("/:id", (req, res) => {
